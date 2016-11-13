cba3e1799ed6dc3bf4b40a19f38decc506e0be85
just nobody <kingdaro@gmail.com>
2016-11-13 00:12:32 -0500

    better graphics lib

--- web/app/directors/drawing.dart
+++ web/app/directors/drawing.dart
@@ -4,19 +4,18 @@ import '../engine/director.dart';
 import '../props.dart';
 import '../graphics.dart';
 
+import '../color.dart';
+
 class DrawingDirector implements Director {
   void direct(List<Actor> actors) {
-    clear();
+    clear(Color.white);
 
     for (final actor in actors) {
       if (actor.has([Position, Size, Appearance])) {
         final pos = actor[Position];
         final size = actor[Size];
-        final color = actor[Appearance];
-
-        canvas.context2D
-          ..fillStyle = color.color
-          ..fillRect(pos.x, pos.y, size.width, size.height);
+        final appearance = actor[Appearance];
+        drawRectangle(pos.x, pos.y, size.width, size.height, appearance.color);
       }
     }
   }
--- web/app/graphics.dart
+++ web/app/graphics.dart
@@ -1,9 +1,81 @@
 import 'dart:html';
+import 'dart:math';
 
-final CanvasElement canvas = querySelector('canvas');
+import 'color.dart';
 
-clear() {
+final CanvasElement canvas = querySelector('#game');
+
+/// Utility class for working with fonts
+/// For use with HTML5 canvas
+class Font {
+  final String family;
+  final num size;
+
+  Font(this.family, this.size);
+
+  String toString() {
+    return '${size}px $family';
+  }
+
+  static roboto(num size) => new Font('Roboto', size);
+}
+
+/// Draws a rectangle
+drawRectangle(num x, num y, num width, num height, [Color color]) {
   canvas.context2D
-    ..fillStyle = 'white'
-    ..fillRect(0, 0, canvas.width, canvas.height);
+    ..save()
+    ..fillStyle = color
+    ..fillRect(x, y, width, height)
+    ..restore();
+}
+
+/// Draws an equilateral polygon
+drawPolygon(num x, num y, num radius, int sides, Color color, [num rotation = 0]) {
+  canvas.context2D
+    ..save()
+    ..beginPath();
+
+  for (var i = 0; i < sides; i++) {
+    final angle = (i / sides) * (PI * 2) + rotation;
+    final ox = cos(angle) * radius;
+    final oy = sin(angle) * radius;
+    canvas.context2D.lineTo(x + ox, y + oy);
+  }
+
+  canvas.context2D
+    ..fillStyle = color
+    ..fill()
+    ..restore();
+}
+
+/// Draw some text
+drawText(String text, num x, num y, Color color, Font font, [String align = 'left']) {
+  canvas.context2D
+    ..save()
+    ..fillStyle = color
+    ..font = font.toString()
+    ..textAlign = align
+    ..fillText(text, x, y)
+    ..restore();
+}
+
+/// Clear the canvas area to a given color
+clear(Color color) {
+  canvas.context2D
+    ..save()
+    ..fillStyle = color
+    ..fillRect(0, 0, canvas.width, canvas.height)
+    ..restore();
+}
+
+/// Reset the canvas transform state
+reset() {
+  canvas.context2D.resetTransform();
+}
+
+/// Perform drawing transforms within a transformation context
+layer(drawOps()) {
+  canvas.context2D.save();
+  drawOps();
+  canvas.context2D.restore();
 }
