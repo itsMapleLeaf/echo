7d6d2a797cde41fb0f62f526adaedef4b9cb5f04
just nobody <kingdaro@gmail.com>
2016-11-14 05:40:38 -0500

    limit # of jumps

--- web/app/game_object.dart
+++ web/app/game_object.dart
@@ -76,20 +76,26 @@ class PlayerInput extends Component {
 
   num walking = 0;
   num walkSpeed = 500;
+  int jumps = 0;
 
   void update(self, dt, world) {
     final vel = self[Velocity];
+    final physics = self[Physics];
 
     num target = 0;
     if (kb.isDown(KeyCode.LEFT)) target -= 1;
     if (kb.isDown(KeyCode.RIGHT)) target += 1;
     walking = target;
 
-    if (kb.wasPressed(KeyCode.UP)) {
+    if (kb.wasPressed(KeyCode.UP) && jumps > 0) {
       vel.vy = -jumpSpeed;
+      jumps--;
     }
-
     vel.vx = lerp(vel.vx, walking * walkSpeed, dt * 16);
+
+    if (physics.hitGround) {
+      jumps = 2;
+    }
   }
 }
 
