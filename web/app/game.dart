98523225cd4384a208842da78c2da7ab1c894047
just nobody <kingdaro@gmail.com>
2016-11-14 03:14:40 -0500

    holy modularity batman

--- web/app/game.dart
+++ web/app/game.dart
@@ -5,19 +5,53 @@ import 'graphics.dart';
 import 'keyboard.dart';
 import 'util.dart';
 
-class Player {
-  final kb = new Keyboard();
+abstract class GameObject {}
 
-  num x = 200;
-  num y = 200;
+abstract class BoundingBox {
+  num x = 0, y = 0, width = 0, height = 0;
+
+  Rectangle get rect => new Rectangle(x, y, width, height);
+
+  void setPosition(num _x, num _y) {
+    x = _x;
+    y = _y;
+  }
+
+  void setSize(num _width, num _height) {
+    width = _width;
+    height = _height;
+  }
+
+  void setCenterPosition(num cx, num cy) {
+    x = cx - width / 2;
+    y = cy - height / 2;
+  }
+}
+
+abstract class Velocity implements BoundingBox {
   num vx = 0;
   num vy = 0;
-  num width = 50;
-  num height = 50;
-  num walkSpeed = 500;
   num gravity = 2500;
+
+  void updatePosition(num dt) {
+    x += vx * dt;
+    y += vy * dt;
+  }
+}
+
+abstract class Jumping implements Velocity {
+  static const jumpSpeed = 800;
+
+  void jump() {
+    vy = -jumpSpeed;
+  }
+}
+
+abstract class PlayerInput implements Jumping {
+  final kb = new Keyboard();
+
   num walking = 0;
-  var color = new Color(0.3, 0.3, 0.3);
+  num walkSpeed = 500;
 
   void respondToInput() {
     num target = 0;
@@ -26,64 +60,101 @@ class Player {
     walking = target;
 
     if (kb.wasPressed(KeyCode.UP)) {
-      vy = -800;
+      jump();
     }
   }
 
-  void updatePosition(num dt) {
+  void updateVelocity(num dt) {
     vx = lerp(vx, walking * walkSpeed, dt * 16);
     vy += gravity * dt;
+  }
+}
 
-    x += vx * dt;
-    y += vy * dt;
+abstract class Collideable implements BoundingBox, Velocity {
+  static Point getCenter(Rectangle rect) {
+    return (rect.topLeft + rect.bottomRight) * 0.5;
   }
 
-  Point getCenter(Rectangle rect) {
-    return (rect.topLeft + rect.bottomRight) * 0.5;
+  static List<Rectangle> sortByDistance(List<Rectangle> rects, Point point) {
+    return new List<Rectangle>.from(rects)
+      ..sort((a, b) {
+        final dist1 = point.distanceTo(getCenter(a));
+        final dist2 = point.distanceTo(getCenter(b));
+        return dist1.compareTo(dist2);
+      });
+  }
+
+  static Rectangle move(Rectangle rect, num dx, num dy) {
+    final left = rect.left + dx;
+    final top = rect.top + dy;
+    return new Rectangle(left, top, rect.width, rect.height);
+  }
+
+  static Point getDisplacement(Rectangle self, List<Rectangle> others) {
+    num dx = 0, dy = 0;
+
+    for (final other in others) {
+      final selfCenter = getCenter(self);
+      final sect = other.intersection(self);
+      if (sect != null) {
+        if (sect.width < sect.height && sect.width != 0) {
+          if (selfCenter.x < getCenter(other).x) {
+            dx -= sect.width;
+          } else {
+            dx += sect.width;
+          }
+        }
+        if (sect.height < sect.width && sect.height != 0) {
+          if (selfCenter.y < getCenter(other).y) {
+            dy -= sect.height;
+          } else {
+            dy += sect.height;
+          }
+        }
+      }
+      self = move(self, dx, dy);
+    }
+
+    return new Point(dx, dy);
   }
 
   void resolveCollisions(List<Rectangle> rects) {
     final selfRect = new Rectangle(x, y, width, height);
     final selfCenter = getCenter(selfRect);
 
-    final closestToFurthest = new List<Rectangle>.from(rects)
-      ..sort((a, b) {
-        final dist1 = selfCenter.distanceTo(getCenter(a));
-        final dist2 = selfCenter.distanceTo(getCenter(b));
-        return dist1.compareTo(dist2);
-      });
+    final closestToFurthest = sortByDistance(rects, selfCenter);
+    final disp = getDisplacement(selfRect, closestToFurthest);
 
-    for (final other in closestToFurthest) {
-      final selfRect = new Rectangle(x, y, width, height);
-      final selfCenter = getCenter(selfRect);
-      final sect = other.intersection(selfRect);
-      if (sect != null) {
-        if (sect.width < sect.height && sect.width != 0) {
-          if (selfCenter.x < getCenter(other).x) {
-            x -= sect.width;
-            if (vx > 0) vx = 0;
-          } else {
-            x += sect.width;
-            if (vx < 0) vx = 0;
-          }
-        }
-        if (sect.height < sect.width && sect.height != 0) {
-          if (selfCenter.y < getCenter(other).y) {
-            y -= sect.height;
-            if (vy > 0) vy = 0;
-          } else {
-            y += sect.height;
-            if (vy < 0) vy = 0;
-          }
-        }
-      }
-    }
+    x += disp.x;
+    y += disp.y;
+
+    if (disp.x != 0 && vx.sign != disp.x.sign) vx = 0;
+    if (disp.y != 0 && vy.sign != disp.y.sign) vy = 0;
   }
+}
+
+abstract class DrawableRect implements BoundingBox {
+  Color color = new Color(0.3, 0.3, 0.3);
 
   void draw() {
     drawRectangle(x, y, width, height, color);
   }
 }
+
+class Player extends GameObject
+with BoundingBox, PlayerInput, Collideable, Jumping, Velocity, DrawableRect {
+  Player() {
+    setSize(50, 50);
+    setCenterPosition(canvas.width / 2, canvas.height / 2);
+  }
+
+  void update(num dt) {
+    respondToInput();
+    updateVelocity(dt);
+    updatePosition(dt);
+  }
+}
+
 
 class Map {
   static const blockSize = 80;
@@ -113,22 +184,24 @@ class Map {
 
 class Game {
   final player = new Player();
+
   final map = new Map([
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '          ',
-    '      1   ',
-    '      1   ',
-    '      1   ',
-    '1111111111',
+    '                ',
+    '                ',
+    '                ',
+    '                ',
+    '                ',
+    '      1         ',
+    '      1         ',
+    '      1         ',
+    '1111111111111111',
   ]);
 
   update(num dt) {
-    player.respondToInput();
-    player.updatePosition(dt);
-    player.resolveCollisions(map.blocks);
+    player.update(dt);
+    if (player is Collideable) {
+      player.resolveCollisions(map.blocks);
+    }
   }
 
   draw() {
